6991ffe04dd2c029db49bfafb3534ebed6b94978
RianIndraP <rianindrapratama2008@gmail.com>
2026-03-28 16:46:20 +0700

    Update model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -26,6 +26,7 @@ class User extends Authenticatable
         'nik',
         'username',
         'email',
+        'foto',
         'password',
         'birth_date',
         'tempat_lahir',
